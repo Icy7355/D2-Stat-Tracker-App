a223a8003bf113bbfb47ccd5d2469864fec24b24
Icy7355 <nathanmoors534@gmail.com>
2025-08-06 15:48:43 +1000

    Adding in the first API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 function App() {
+  var apiKey = "1ec44620f32d4d25b33fb77d12b9e421";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+  xhr.setRequestHeader("X-API-Key", apiKey);
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      var json = JSON.parse(this.responseText);
+      console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
+    }
+  }
+
+  xhr.send();
+
   const value = 'World';
   return <div>Hello {value}</div>;
 }
